80cc758243a5dfa494693e7cd2b7cc8c7edf6403
Levir Olea <Levir777@outlook.com>
2022-12-23 16:20:52 -0600

    avance de pagina web, se agregó la vista de about con sus estilos definidos

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,16 +3,48 @@
 </script>
 
 <template>
-  <section>
-    <picture>
-      <img src="../assets/img/" alt="">
-    </picture>
+  <section class="img-header">
+    <figure>
+      <img src="../assets/img/about-us-img-principal.jpg" alt="">
+    </figure>
   </section>
-  <section>
-
-  </section>
-  <section>
-
+  <section class="about-us-container">
+    <nav class="nav-page-info">
+      <router-link class="nav-item-page" to="/">Home</router-link>
+      <p>></p>
+      <router-link class="nav-item-page" to="/about">About Us</router-link>
+    </nav>
+    <div class="main-text-container">
+      <figure>
+        <img src="../assets/img/img-buildings.jpg" alt="">
+      </figure>
+      <article class="about-us-text">
+        <h2>ABOUT US</h2>
+        <p>Gateway Fabrication Solutions was formed in early 2017 as the headquarters for a group of four manufacturing
+          and
+          processing plants dedicated to metal transforming. These plants started operations in 2001 and currently
+          maintain a
+          total footprint of 160,000 square feet with more than 370 employees. Our plants utilize Lean Manufacturing
+          processes
+          and certified quality systems, including ISO 9001:2015.</p>
+        <p>Today, Gateway Fabrication Solutions has operations in the United States and Mexico with manufacturing and
+          distribution capabilities in both countries, making GFS the perfect solution for all of your stamping,
+          machining,
+          tube forming, and assembling / kitting needs.
+        </p>
+        <h3>Location:</h3>
+        <div class="about-us-contact-container">
+          <h4>Mexico Facilities</h4>
+          <p>Ing. Tomas Limon Gutierrez 123 <br>
+            Zona Industrial Sur <br>
+            Guadalajara, Jal. CP 44940 <br>
+            Mexico <br>
+            P:
+            <a href="tel:52-338-995-0263">+52 338.995.0263</a>
+          </p>
+        </div>
+      </article>
+    </div>
   </section>
   <section>
 
