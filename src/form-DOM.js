485517cf2f45264071974b2a67c71987577be604
Levir7 <Levir777@outlook.com>
2023-03-06 16:17:35 -0600

    test php

--- src/form-DOM.js
+++ src/form-DOM.js
@@ -1,43 +1,43 @@
-// const formItemFullName = document.querySelector('#form-labelFN')
-// const formCompanyName = document.querySelector('#form-labelCN')
-// const formEmailAddress = document.querySelector('#form-labelEA')
-// const formSelect = document.querySelector('#form-labelSelect')
-// const formComments = document.querySelector('#form-labelComments')
-// const textFullName = document.querySelector('#form-fullName')
-// const textCompanyName = document.querySelector('#form-companyName')
-// const textEmailAddress = document.querySelector('#form-emailAddress')
-// const textSelect = document.querySelector('#form-select')
-// const textComments = document.querySelector('#form-comments')
+const formItemFullName = document.querySelector('#form-labelFN')
+const formCompanyName = document.querySelector('#form-labelCN')
+const formEmailAddress = document.querySelector('#form-labelEA')
+const formSelect = document.querySelector('#form-labelSelect')
+const formComments = document.querySelector('#form-labelComments')
+const textFullName = document.querySelector('#form-fullName')
+const textCompanyName = document.querySelector('#form-companyName')
+const textEmailAddress = document.querySelector('#form-emailAddress')
+const textSelect = document.querySelector('#form-select')
+const textComments = document.querySelector('#form-comments')
 
-// textFullName.addEventListener('click', toggleFullName);
-// textCompanyName.addEventListener('click', labelCompanyName);
-// textEmailAddress.addEventListener('click', labelEmailAddress);
-// textSelect.addEventListener('click', labelSelect);
-// textComments.addEventListener('click', labelComments);
+textFullName.addEventListener('click', toggleFullName);
+textCompanyName.addEventListener('click', labelCompanyName);
+textEmailAddress.addEventListener('click', labelEmailAddress);
+textSelect.addEventListener('click', labelSelect);
+textComments.addEventListener('click', labelComments);
 
 
-// function toggleFullName (){
-//     if(!formItemFullName.classList.contains('formLabel')){
-//         formItemFullName.classList.add('formLabel')
-//     } 
-// }
-// function labelCompanyName (){
-//     if(!formCompanyName.classList.contains('formLabel')){
-//         formCompanyName.classList.add('formLabel')
-//     }
-// }
-// function labelEmailAddress (){
-//     if(!formEmailAddress.classList.contains('formLabel')){
-//         formEmailAddress.classList.add('formLabel')
-//     }
-// }
-// function labelSelect(){
-//     if(!formSelect.classList.contains('formLabel')){
-//         formSelect.classList.add('formLabel')
-//     }
-// }
-// function labelComments(){
-//     if(!formComments.classList.contains('formLabel')){
-//         formComments.classList.add('formLabel')
-//     }
-// }
+function toggleFullName (){
+    if(!formItemFullName.classList.contains('formLabel')){
+        return formItemFullName.classList.add('formLabel');
+    } 
+}
+function labelCompanyName (){
+    if(!formCompanyName.classList.contains('formLabel')){
+        return formCompanyName.classList.add('formLabel');
+    }
+}
+function labelEmailAddress (){
+    if(!formEmailAddress.classList.contains('formLabel')){
+        return formEmailAddress.classList.add('formLabel');
+    }
+}
+function labelSelect(){
+    if(!formSelect.classList.contains('formLabel')){
+        return formSelect.classList.add('formLabel');
+    }
+}
+function labelComments(){
+    if(!formComments.classList.contains('formLabel')){
+        return formComments.classList.add('formLabel');
+    }
+}
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,29 +25,29 @@
       </article>
     </div>
 
-    <form action="enviar.php" method="POST" class="main-form-container">
+    <form action="enviar.php" method="post" class="main-form-container">
       <div id="row-form1" class="row-form form-item">
         <ul>
           <li id="form-text-FullName">
-            <label for="form-fullName" id="form-labelFN" name="fullname">Full Name</label>
-            <input type="text" id="form-fullName" class="form-item" />
+            <!-- <label for="form-fullName" id="form-labelFN" name="fullname">Full Name</label> -->
+            <input type="text" name="fullName" id="form-fullName" class="form-item" placeholder="Full Name" required/>
           </li>
           <li id="form-text-CompanyName">
-            <label for="form-companyName" id="form-labelCN">Company Name</label>
-            <input type="text" id="form-companyName" class="form-item" name="companyname"/>
+            <!-- <label for="form-companyName" id="form-labelCN">Company Name</label> -->
+            <input type="text" id="form-companyName" class="form-item" name="companyname" placeholder="Company Name"/>
           </li>
           <li id="form-text-EmailAddress">
-            <label for="form-emailAddress" id="form-labelEA" >Email Address</label>
-            <input type="email" id="form-emailAddress" class="form-item" name="email" required/>
+            <!-- <label for="form-emailAddress" id="form-labelEA" >Email Address</label> -->
+            <input type="email" id="form-emailAddress" class="form-item" name="email" placeholder="Email Address" required/>
           </li>
         </ul>
       </div>
       <div id="row-form2" class="row-form">
         <ul>
           <li id="form-select-service">
-            <label for="form-select" id="form-labelSelect">Service of Interest</label>
-            <select name="service " id="form-select" class="form-item" placeholder="Service of Interest">
-              <option value=""></option>
+            <!-- <label for="form-select" id="form-labelSelect" class="labelSelectForm">Service of Interest</label> -->
+            <select name="service " id="form-select" class="form-item form-select-options" placeholder="Service of Interest" required>
+              <option hidden selected>Service of Interest</option>
               <option value="General Inquery">General Inquery</option>
               <option value="Metal Stamping">Metal Stamping</option>
               <option value="Tube Forming">Tube Forming</option>
@@ -67,13 +67,14 @@
             ></textarea>
           </li>
           <div class="form-button-container">
+            <label for="" class="labelForm">Upload file or drawing(up to 10 files @ 100 MB each)</label>
             <li id="form-uploadFile">
               <div for="" class="form-button">
                 <input type="file" value="" id="form-button-file" name=""/>
               </div>
             </li>
             <li id="form-submitForm">           
-              <div class="form-button">
+              <div class="form-button submit">
                 <input type="submit" value="Submit Form" id="form-button-submit"/>
               </div>     
             </li>
